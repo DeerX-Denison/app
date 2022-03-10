c61c9fe1997e80de0a532fba5dd3256d792b54c7
Wilson <43991506+WilsonLe@users.noreply.github.com>
2022-03-10 00:20:35 -0500

    update light badge component UI

--- src/Components/Badges/Light.tsx
+++ src/Components/Badges/Light.tsx
@@ -4,11 +4,14 @@ import { View } from 'react-native';
 
 interface Props {}
 
+/**
+ * light badge
+ */
 const Light: FC<Props> = ({ children }) => {
 	return (
 		<View
 			style={tw(
-				'flex-row border mx-2 my-1 items-center py-0.5 px-1 rounded-full bg-yellow-100'
+				'flex-row border mx-2 my-1 items-center py-0.5 px-1 rounded-full bg-blue-100'
 			)}
 		>
 			{children}

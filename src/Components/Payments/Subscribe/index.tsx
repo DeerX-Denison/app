a363920f9c1315a0283191e024da6f91d9b7acc4
Wilson <43991506+WilsonLe@users.noreply.github.com>
2023-02-18 08:08:35 -0500

    remove stripe

--- src/Components/Payments/Subscribe/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-
-interface Props {}
-
-const Subscribe: FC<Props> = () => {
-	return <></>;
-};
-
-export default Subscribe;
--- src/Components/Payments/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as CardForm } from './CardForm';
-export { default as Subscribe } from './Subscribe';
